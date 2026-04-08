5dfa23447c4ee6725b977bc2abb66e936e1dac36
qiao13822919184-byte <qiao13822919184@gmail.com>
2026-04-08 11:00:08 +0800

    fix: 修复 GitHub Pages 子路径跳转 404 问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 1. 你的 GitHub Pages 主域名
+  site: 'https://qiao13822919184-byte.github.io', 
+  // 2. 你的仓库名字（必须以斜杠开头！）
+  base: '/test', 
+});
